5905afa3b1f2ff5f16d2e379bc63069716e30e9d
mhuggins <matt.huggins@gmail.com>
2016-02-29 20:47:04 -0700

    Deprecated circuitry-middleware

--- lib/circuitry/middleware.rb
+++ lib/circuitry/middleware.rb
@@ -1,3 +1,5 @@
+warn '[DEPRECATION] circuitry-middleware has been renamed to circuitry-rails and will no longer be supported. Please upgrade to circuitry-rails as soon as possible.'
+
 require 'circuitry/middleware/version'
 require 'circuitry/middleware/rack'
 require 'circuitry/middleware/railtie' if defined?(Rails)
